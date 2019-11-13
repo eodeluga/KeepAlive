ed0bd753bd8dcdbe2503491d5a9d6e0441c3b45a
Eugene Odeluga <eo15_da@ANGLIA.LOCAL>
2019-11-13 04:20:01 +0000

    First commit

---
+++ KeepAlive.cs
@@ -0,0 +1,8 @@
+﻿// Command line tool to keep computer alive and prevent sleep
+namespace KeepAlive {
+    class KeepAlive {
+        static void Main(string[] args) {
+            PowerHelper.ForceSystemAwake();
+        }
+    }
+}
